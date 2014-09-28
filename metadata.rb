404cb229e998c9aea3b5066f723bb5d581a043d7
Nik Ormseth <nik@ormseth.net>
2014-09-28 07:25:05 -0700

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'microblog_deploy'
+maintainer       'Level 11'
+maintainer_email 'nikormseth@level11.com'
+license          ''
+description      'Installs/Configures microblog_deploy'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: microblog_deploy
+# Recipe:: default
+#
+# Copyright 2014, Level 11
+#
